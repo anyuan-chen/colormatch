05bcb0adbc9ef1f44e71bbda0c18f1eecaabae2d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-10-25 19:14:18 -0400

    added buf and generated protos

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
 package cmd
 
-import "google.golang.org/grpc"
+//import "google.golang.org/grpc"
 
 func main() {
-	grpcServer := grpc.NewServer()
+	//grpcServer := grpc.NewServer()
 	//grpcServer.RegisterService()
 }
