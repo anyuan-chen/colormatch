c80650964895376eb0ba29cdc84e5a69f4846858
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-10-26 23:47:00 -0400

    completed match color

--- pkg/color_service/colors.go
+++ pkg/color_service/colors.go
@@ -2,6 +2,8 @@ package color_service
 
 import (
 	"context"
+	"errors"
+	"math"
 
 	"github.com/anyuan-chen/colormatch/protos/colors/v1"
 )
@@ -9,29 +11,22 @@ import (
 type ColorServiceServer struct {
 	colors.UnimplementedPaletteMatchingServiceServer
 }
-type XYZ struct {
-	x float64
-	y float64
-	z float64
-}
-type CIE76 struct {
-	l float64
-	a float64
-	b float64
-}
 
 func (s *ColorServiceServer) MatchColor(ctx context.Context, req *colors.MatchColorRequest) (*colors.MatchColorResponse, error) {
 	user_defined_color := req.GetColor()
 	palette := req.GetPalette().Color
 
+	dist := math.MaxFloat64
+	var closestColor *colors.Color
 	for _, color := range palette {
-		
+		cur_dist, err := HexColorDifference(color.HexCode, user_defined_color.HexCode)
+		if err != nil {
+			return nil, errors.New("")
+		}
+		if cur_dist > dist {
+			dist = cur_dist
+			closestColor = color
+		}
 	}
-
-	//hex -> rgb
-	//rgb -> l*a*b*
-	//l*a*b* -> euclidian distance between l, a, and b
-	//shortest euclidian distance "wins"
-
-	return &colors.MatchColorResponse{Color: &colors.Color{HexCode: "hi"}}, nil
+	return &colors.MatchColorResponse{Color: closestColor}, nil
 }
