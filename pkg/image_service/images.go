4b8cf117f7b689a356229defb760741ed7f6b966
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-11-01 14:22:49 -0400

    background color retriever

--- pkg/image_service/images.go
+++ pkg/image_service/images.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"image/color"
+	"math"
 
 	colorsv1 "github.com/anyuan-chen/colormatch/gen/proto/go/colors/v1"
 	imagev1 "github.com/anyuan-chen/colormatch/gen/proto/go/images/v1"
@@ -24,13 +25,23 @@ func (iss *PaletteMatchingServer) GetBackgroundColor(ctx context.Context, req *i
 	imgData := req.Image
 	palette := req.Palette
 	backgroundPalette := req.BackgroundColors
-	closest_palette, err := GetDistanceMatrix(imgData, palette, iss.Color_Service)
+	closest_palette, err := GetFrequencyArray(imgData, palette, iss.Color_Service)
+	closest_background_palette := make(map[*sharedv1.Color]int32)
+	for _, backgroundColor := range backgroundPalette.Color {
+		closest_background_palette[backgroundColor] = closest_palette[backgroundColor]
+	}
+	var maxKey *sharedv1.Color = &sharedv1.Color{R: 0, G: 0, B: 0, A: 1}
+	var maxVal int32 = math.MinInt32
+	for key, val := range closest_background_palette {
+		if val > maxVal {
+			maxVal = val
+			maxKey = key
+		}
+	}
 	if err != nil {
 		return error_response, err
 	}
-	
-	var backgroundColor *sharedv1.Color
-	return &imagev1.GetBackgroundColorResponse{Color: backgroundColor}, nil
+	return &imagev1.GetBackgroundColorResponse{Color: maxKey}, nil
 }
 
 func (iss *PaletteMatchingServer) GetHighlightColor(ctx context.Context, req *imagev1.GetHighlightColorRequest) (*imagev1.GetHighlightColorResponse, error) {
