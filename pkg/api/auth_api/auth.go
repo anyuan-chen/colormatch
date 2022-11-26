bc99d5b3cbec775acf82fdc984221a52051b2b96
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-11-25 21:16:28 -0500

    authentication endpoints for api

--- pkg/api/auth_api/auth.go
+++ pkg/api/auth_api/auth.go
@@ -1,4 +1,11 @@
 package api_auth
 
+import (
+	"github.com/anyuan-chen/colormatch/pkg/session_management_service"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
 type AuthAPI struct {
+	SpotifyAuthenticator spotifyauth.Authenticator
+	session_manager      session_management_service.Session_Management_Service
 }
--- pkg/api/auth_api/spotify.go
+++ pkg/api/auth_api/spotify.go
@@ -1,10 +1,55 @@
 package api_auth
 
-import "net/http"
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
+	session_managementv1 "github.com/anyuan-chen/colormatch/gen/proto/go/session_management/v1"
+)
 
 func (a *AuthAPI) Login(w http.ResponseWriter, r *http.Request) {
-
+	expiration := time.Now().Add(20 * time.Minute)
+	state := make(map[string]interface{})
+	randomizedState := make([]byte, 16)
+	rand.Read(randomizedState)
+	state["random"] = randomizedState
+	json, err := json.Marshal(state)
+	if err != nil {
+		http.Error(w, "A JSON Encoding error has been encountered.", http.StatusInternalServerError)
+	}
+	encoded_json := base64.URLEncoding.EncodeToString(json)
+	url := a.SpotifyAuthenticator.AuthURL(encoded_json)
+	cookie := http.Cookie{Name: "oauthstate", Value: encoded_json, Expires: expiration}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 func (a *AuthAPI) Callback(w http.ResponseWriter, r *http.Request) {
-
+	state, err := r.Cookie("oauthstate")
+	if err != nil || r.FormValue("state") != state.Value {
+		http.Error(w, "Bad OAuth State", http.StatusInternalServerError)
+	}
+	token, err := a.SpotifyAuthenticator.Token(r.Context(), state.Value, r)
+	if err != nil {
+		http.Error(w, "Failed to Retrieve Token", http.StatusInternalServerError)
+	}
+	token_json, err := json.Marshal(token)
+	if err != nil {
+		http.Error(w, "Error encoding JSON token", http.StatusInternalServerError)
+	}
+	set_token_request := &session_managementv1.SetTokenRequest{
+		Token: token_json,
+	}
+	set_token_response, err := a.session_manager.SetToken(context.Background(), set_token_request)
+	if err != nil {
+		http.Error(w, "Problem with the session management service: "+err.Error(), http.StatusInternalServerError)
+	}
+	cookie := http.Cookie{Name: os.Getenv("spotify_session_id"), Value: set_token_response.Ciphertext, SameSite: http.SameSiteNoneMode, Secure: true}
+	http.SetCookie(w, &cookie)
+	//todo later
+	http.Redirect(w, r, os.Getenv(""), http.StatusPermanentRedirect)
 }
