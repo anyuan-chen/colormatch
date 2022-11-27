3b3a59de6d09dd176d42b3eda7334aadc3cbce8f
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-11-27 17:15:37 -0500

    fixed spotify endpoints

--- pkg/api/auth_api/auth.go
+++ pkg/api/auth_api/auth.go
@@ -1,11 +1,9 @@
 package api_auth
 
 import (
-	"github.com/anyuan-chen/colormatch/pkg/session_management_service"
-	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	session_managementv1 "github.com/anyuan-chen/colormatch/gen/proto/go/session_management/v1"
 )
 
 type AuthAPI struct {
-	SpotifyAuthenticator spotifyauth.Authenticator
-	session_manager      session_management_service.Session_Management_Service
+	Session_Manager      session_managementv1.SessionManagementServiceClient
 }
--- pkg/api/auth_api/spotify.go
+++ pkg/api/auth_api/spotify.go
@@ -5,14 +5,20 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	session_managementv1 "github.com/anyuan-chen/colormatch/gen/proto/go/session_management/v1"
+	"github.com/zmb3/spotify"
 )
 
+var Authenticator = spotify.NewAuthenticator(os.Getenv("SPOTIFY_REDIRECT_URI"), spotify.ScopeUserReadPrivate)
+
 func (a *AuthAPI) Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("redirect uri:", os.Getenv("SPOTIFY_REDIRECT_URI"))
+	fmt.Println("login endpoint hit")
 	expiration := time.Now().Add(20 * time.Minute)
 	state := make(map[string]interface{})
 	randomizedState := make([]byte, 16)
@@ -21,35 +27,48 @@ func (a *AuthAPI) Login(w http.ResponseWriter, r *http.Request) {
 	json, err := json.Marshal(state)
 	if err != nil {
 		http.Error(w, "A JSON Encoding error has been encountered.", http.StatusInternalServerError)
+		return
 	}
 	encoded_json := base64.URLEncoding.EncodeToString(json)
-	url := a.SpotifyAuthenticator.AuthURL(encoded_json)
+	fmt.Println("login: json state created " + encoded_json)
+	url := Authenticator.AuthURL(encoded_json)
+	fmt.Println("login: authurl created " + url)
 	cookie := http.Cookie{Name: "oauthstate", Value: encoded_json, Expires: expiration}
 	http.SetCookie(w, &cookie)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 func (a *AuthAPI) Callback(w http.ResponseWriter, r *http.Request) {
 	state, err := r.Cookie("oauthstate")
+	fmt.Println("cookie state: " + state.Value)
+	fmt.Println("form value: " + r.FormValue("state"))
 	if err != nil || r.FormValue("state") != state.Value {
 		http.Error(w, "Bad OAuth State", http.StatusInternalServerError)
+		return
 	}
-	token, err := a.SpotifyAuthenticator.Token(r.Context(), state.Value, r)
+	token, err := Authenticator.Token(r.FormValue("state"), r)
+	fmt.Println("Authenticator Token error:", err)
 	if err != nil {
-		http.Error(w, "Failed to Retrieve Token", http.StatusInternalServerError)
+		http.Error(w, "Failed to Retrieve Token "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 	token_json, err := json.Marshal(token)
 	if err != nil {
 		http.Error(w, "Error encoding JSON token", http.StatusInternalServerError)
+		return
 	}
-	set_token_request := &session_managementv1.SetTokenRequest{
+	fmt.Println("Token", token)
+	set_token_request := session_managementv1.SetTokenRequest{
 		Token: token_json,
 	}
-	set_token_response, err := a.session_manager.SetToken(context.Background(), set_token_request)
+	set_token_response, err := a.Session_Manager.SetToken(context.Background(), &set_token_request)
+	fmt.Println("Final Code: ", set_token_response.Ciphertext)
 	if err != nil {
 		http.Error(w, "Problem with the session management service: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 	cookie := http.Cookie{Name: os.Getenv("spotify_session_id"), Value: set_token_response.Ciphertext, SameSite: http.SameSiteNoneMode, Secure: true}
+
 	http.SetCookie(w, &cookie)
 	//todo later
-	http.Redirect(w, r, os.Getenv(""), http.StatusPermanentRedirect)
+	http.Redirect(w, r, os.Getenv("FRONTEND_SPOTIFY_CALLBACK_URL"), http.StatusPermanentRedirect)
 }
